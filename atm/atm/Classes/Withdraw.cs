155c021037cd9aa0503cfe8a879e7fff49a8cccd
jcqnly <jcqnly@gmail.com>
2018-08-23 09:58:38 -0700

    add classes for add, deposit and view balance

---
+++ atm/atm/Classes/Withdraw.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace atm.Classes
+{
+	public class Withdraw
+	{
+	}
+}
---
+++ atm/atm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace atm
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
